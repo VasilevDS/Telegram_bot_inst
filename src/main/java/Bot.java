25cb7b0e875fd40fb469ca0be0408c50a808f6aa
vasilev <vasilyev0992@gmail.com>
2020-02-18 18:06:52 +0300

    переноса мейна в класс бот

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,9 +1,24 @@
+import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Bot extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegram = new TelegramBotsApi();
+
+        Bot bot = new Bot();
+        try {
+            telegram.registerBot(bot);
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public void onUpdateReceived(Update update) {
--- src/main/java/StartBot.java
+++ src/main/java/StartBot.java
@@ -2,8 +2,8 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-public class Main {
-    public static void main(String[] args) {
+public class StartBot {
+    /*public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegram = new TelegramBotsApi();
 
@@ -13,5 +13,5 @@ public class Main {
         } catch (TelegramApiRequestException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 }
